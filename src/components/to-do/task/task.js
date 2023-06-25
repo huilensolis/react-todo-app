ba8f89f6e14d4f44517e4d68e2db3e26909f71b2
Huilen Solis <xywvanz@gmail.com>
2023-06-25 19:20:08 -0300

    css design for task deleting done

--- src/components/to-do/task/task.js
+++ src/components/to-do/task/task.js
@@ -1,13 +1,22 @@
 import "./styles.css";
+import { useState } from "react";
 
 function TodoTask({ title, completed, completeTaskFunction }) {
 
   function setTaskCompleted() {
     completeTaskFunction(title)
   }
+  
+  const [isHovering, setIsHovering] = useState(null)
 
+  function handleMouseEnter(title){
+    setIsHovering(title)
+  }
+  function handleMouseLeave(){
+    setIsHovering(null)
+  }
   return (
-    <li>
+    <li className={isHovering === title ? "maybe-delete-background-red" : ""}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         height="1em"
@@ -25,7 +34,7 @@ function TodoTask({ title, completed, completeTaskFunction }) {
           {title}
         </h1>
       </main>
-      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" className="completed-task-icon icon">
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" className="delete-task-icon icon" onMouseEnter={() => handleMouseEnter(title)} onMouseLeave={handleMouseLeave}>
         <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
       </svg>
     </li>
