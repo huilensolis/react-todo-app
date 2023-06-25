bce5d483fa435e378643b1758c97e6215e6173f3
Huilen Solis <xywvanz@gmail.com>
2023-06-25 19:49:09 -0300

    alternative texts in the side section depending if you have compelted every task or the tasks are 0

--- src/components/to-do/count/count.js
+++ src/components/to-do/count/count.js
@@ -1,8 +1,22 @@
 import './styles.css'
 
 function CompletedTaskCount({completedTaskCount, totalTaskCount, taskOrTasks}) {
+
+
+  let firstLineContent = 'You have completed'
+  let secondLineContent = `${completedTaskCount} of ${totalTaskCount} ${taskOrTasks}`
+
+  if(completedTaskCount === totalTaskCount && totalTaskCount > 0){
+    firstLineContent = 'Good job! 👏'
+    secondLineContent = 'You have completed all your tasks for today'
+  } else if(totalTaskCount === 0){
+    firstLineContent = 'it looks a kinda empty here 🐻'
+    secondLineContent = 'Try creating some new tasks!'
+  }
+  
   return (
-    <h1 className='count-h1'>You have completed <br /> <span> { completedTaskCount } of { totalTaskCount } {taskOrTasks} </span> </h1>
+    <h1 className='count-h1'>{firstLineContent} <br /> <span> {secondLineContent} </span> </h1>
   );
 }
-export { CompletedTaskCount }
+
+export {CompletedTaskCount}
