730fe8ba5f4ca3b5a720b0470d6038e333326cf8
Huilen Solis <xywvanz@gmail.com>
2023-06-27 18:58:51 -0300

    feature: custom hook to interact with the local storage

--- src/App.js
+++ src/App.js
@@ -10,76 +10,100 @@ import { TodoTask } from "./components/to-do/task/task";
 import { CreateTaskBtn } from "./components/to-do/create-task/create-task";
 
 function App() {
+  // tasks logic
+  function useLocalStorage(localStorageKey) {
+    const [items, setItems] = useState(() => {
+      const ItemsFromLocalStorage = localStorage.getItem(localStorageKey);
 
-  const [tasks, setTasks] = useState(() => {
-    const tasksFromLocalStorage = localStorage.getItem('tasks_V1')
-    if(tasksFromLocalStorage){
-      return JSON.parse(tasksFromLocalStorage)
-    } else{
-      return []
+      if (ItemsFromLocalStorage) {
+        return JSON.parse(ItemsFromLocalStorage);
+      }
+
+      return [];
+    });
+
+    function getItem() {
+      return items;
     }
-  })
 
-  function saveTasksToLocalStorage(tasksArray){
-    window.localStorage.setItem('tasks_V1', JSON.stringify(tasksArray))
+    function saveItemsToLocalStorage(items) {
+      localStorage.setItem(localStorageKey, JSON.stringify(items));
+      setItems(items);
+    }
+    return { saveItemsToLocalStorage, getItem };
+  }
+  const {
+    getItem: getTasks,
+    saveItemsToLocalStorage: saveTasksToLocalStorage,
+  } = useLocalStorage("tasks_V1");
+
+  function completeTask(taskTitle) {
+    const taskIndex = getTasks().findIndex((task) => task.title === taskTitle);
+
+    if (taskIndex >= 0) {
+      const newTaskList = [...getTasks()];
+      newTaskList[taskIndex].completed = !newTaskList[taskIndex].completed;
+      newTaskList.sort((task1, task2) => {
+        if (!task1.completed && task2.completed) {
+          return -2;
+        } else {
+          return 0;
+        }
+      });
+      saveTasksToLocalStorage(newTaskList);
+      console.log(getTasks());
+    } else {
+      console.log("task not found");
+    }
   }
 
+  function deleteTask(taskTitle) {
+    const indexOfTask = getTasks().findIndex((task) => task.title === taskTitle);
+    const newTasksArray = [...getTasks()];
+    newTasksArray.splice(indexOfTask, 1);
+    saveTasksToLocalStorage(newTasksArray);
+  }
   // search logic
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
-  const tasksSearched = tasks.filter(task => {
+  const tasksSearched = getTasks().filter((task) => {
     const searchText = searchValue.toLowerCase();
     const taskTitle = task.title.toLowerCase();
 
-    return taskTitle.includes(searchText)
-  })
+    return taskTitle.includes(searchText);
+  });
+
+  // doom
+  let totalTask = getTasks().length;
+  let taskCompleted = getTasks().filter((task) => !!task.completed).length;
 
   let tasksContent;
-  if(searchValue === ''){
-    tasksContent = tasks.map( task => (
-      <TodoTask title={task.title} key={task.title} completed={task.completed} completeTaskFunction={completeTask} deleteTaskFunction={deleteTask}/>
-    ))
-  } else{
-    tasksContent = tasksContent = tasksSearched.map( task => (
-      <TodoTask title={task.title} key={task.title} completed={task.completed} completeTaskFunction={completeTask}/>
-    ))
+  if (searchValue === "") {
+    tasksContent = getTasks().map((task) => (
+      <TodoTask
+        title={task.title}
+        key={task.title}
+        completed={task.completed}
+        completeTaskFunction={completeTask}
+        deleteTaskFunction={deleteTask}
+      />
+    ));
+  } else {
+    tasksContent = tasksContent = tasksSearched.map((task) => (
+      <TodoTask
+        title={task.title}
+        key={task.title}
+        completed={task.completed}
+        completeTaskFunction={completeTask}
+      />
+    ));
   }
 
-  let totalTask = tasks.length
-  let taskCompleted = tasks.filter(task => !!task.completed).length
-
-  function completeTask(taskTitle){
-    const taskIndex = tasks.findIndex(task => task.title === taskTitle)
-
-    if(taskIndex >= 0){
-        const newTaskList = [...tasks]
-        newTaskList[taskIndex].completed = !newTaskList[taskIndex].completed
-        newTaskList.sort((task1, task2) => {
-          if(!task1.completed && task2.completed){
-            return -2
-          } else{
-            return 0
-          }
-        })
-        setTasks(newTaskList)
-        saveTasksToLocalStorage(newTaskList)
-    } else{
-      console.log("task not found")
-    }
-  }
-  function deleteTask(taskTitle){
-    const indexOfTask = tasks.findIndex(task => task.title === taskTitle)
-    const newTasksArray = [...tasks]
-    newTasksArray.splice(indexOfTask, 1)
-    setTasks(newTasksArray)
-    saveTasksToLocalStorage(newTasksArray)
-  }
-
-  function taskOrTasks(){
-    if(totalTask > 1){
-      return 'tasks'
-    } else{
-      return 'task'
+  function taskOrTasks() {
+    if (totalTask > 1) {
+      return "tasks";
+    } else {
+      return "task";
     }
   }
 
@@ -87,27 +111,23 @@ function App() {
     // <React.Fragment>
     <main className="main-container">
       <section className="aside-section">
-        
         <div className="child-1">
-
           <h1 id="title">Todo-List</h1>
-          <CompletedTaskCount completedTaskCount={taskCompleted} totalTaskCount={totalTask} taskOrTasks={taskOrTasks()}/>
+          <CompletedTaskCount
+            completedTaskCount={taskCompleted}
+            totalTaskCount={totalTask}
+            taskOrTasks={taskOrTasks()}
+          />
           <Nav searchValue={searchValue} setSearchValue={setSearchValue} />
-
         </div>
 
         <div className="child-2">
-
           <CreateTaskBtn class="createTaskBtn" />
-
         </div>
-
       </section>
 
       <section className="task-section">
-        <TaskList>
-          {tasksContent}
-        </TaskList>
+        <TaskList>{tasksContent}</TaskList>
       </section>
     </main>
     // </React.Fragment>
